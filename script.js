cd84b405afabcd5f4dba94a9ec0e6dede3f7a726
Chashmat un Nasim <chashmatnasim@gmail.com>
2022-07-24 16:53:22 +0500

    trying to do something

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
-document.head.innerHTML += `<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />`;
+let chance = [false, true, false, false];
 
-document.body.innerHTML += `
-<div class=cover>
-      <span class="material-symbols-outlined">
-            close
-      </span>
-      <div class="slider">
-            <span style="--i:1"><img src="./images/1.jpg" alt="mountain"></span>
-            <span style="--i:2"><img src="./images/2.jpg" alt="mountain"></span>
-            <span style="--i:3"><img src="./images/3.jpg" alt="mountain"></span>
-            <span style="--i:4"><img src="./images/4.jpg" alt="mountain"></span>
-            <span style="--i:5"><img src="./images/5.jpg" alt="mountain"></span>
-      </div>
-      <div class="btn">
-            <span id="1" class="active"></span>
-            <span id="2"></span>
-            <span id="3"></span>
-            <span id="4"></span>
-            <span id="5"></span>
-      </div>
-</div>`;
+let selected = chance[Math.floor(Math.random() * chance.length)];
 
-let rotation = 0;
-let pictures = document.querySelectorAll("div.slider span");
-let button = document.querySelectorAll("div.btn span");
-let distance = 360 / pictures.length;
-document.documentElement.style.setProperty("--d", distance);
-console.log(pictures, button);
+console.log(selected);
 
-setInterval(() => {
-      rotation += distance;
-      document.documentElement.style.setProperty("--r", `${rotation}deg`);
-      for (let i = 0; i < button.length; i++) {
-            if (button[i].classList[0] == 'active') {
-                  button[i].classList.remove('active');
-            }
-      }
-      button[rotation / distance - (5 * Math.floor((rotation / distance) / pictures.length))].classList.add("active");
-}, 1500);
+if (selected) slideShow();
 
-for (let i = 0; i < button.length; i++) {
-      button[i].addEventListener("click", (e) => {
-            let id = e.target.id;
-            rotation = (id - 1) * distance;
-            document.documentElement.style.setProperty("--r", `${rotation}deg`);
-            for (let i = 0; i < button.length; i++) {
-                  if (button[i].classList[0] == 'active') {
-                        button[i].classList.remove('active');
+function slideShow() {
+      let abcdefghi = 0;
+      let abcdefghik = document.querySelectorAll("div.abcdefghijklmnopqrstuvwxy span");
+      let abcdefghikl = document.querySelectorAll("div.abcdefghijklmnopqrstuvwx span");
+      let abcdefghiklm = 360 / abcdefghik.length;
+      document.documentElement.style.setProperty("--d", abcdefghiklm);
+      console.log(abcdefghik, abcdefghikl);
+
+      const animation = setInterval(() => {
+            abcdefghi += abcdefghiklm;
+            document.documentElement.style.setProperty("--r", `${abcdefghi}deg`);
+            for (let i = 0; i < abcdefghikl.length; i++) {
+                  if (abcdefghikl[i].classList[0] == 'abcdefghiklmn') {
+                        abcdefghikl[i].classList.remove('abcdefghiklmn');
                   }
             }
-            button[id - 1].classList.add("active");
+            abcdefghikl[abcdefghi / abcdefghiklm - (5 * Math.floor((abcdefghi / abcdefghiklm) / abcdefghik.length))].classList.add("abcdefghiklmn");
+      }, 1500);
+
+      for (let i = 0; i < abcdefghikl.length; i++) {
+            abcdefghikl[i].addEventListener("click", (e) => {
+                  let id = e.target.id;
+                  abcdefghi = (id - 1) * abcdefghiklm;
+                  document.documentElement.style.setProperty("--r", `${abcdefghi}deg`);
+                  for (let i = 0; i < abcdefghikl.length; i++) {
+                        if (abcdefghikl[i].classList[0] == 'abcdefghiklmn') {
+                              abcdefghikl[i].classList.remove('abcdefghiklmn');
+                        }
+                  }
+                  abcdefghikl[id - 1].classList.add("abcdefghiklmn");
+            });
+      }
+
+      document.querySelector(".abcdefghijklmnopqrstuvw").addEventListener("click", () => {
+            document.head.innerHTML += `
+            <style>
+                  body {
+                        overflow: auto !important;
+                        user-select: auto !important;
+                  }
+
+                  ::webkit-scorllbar {
+                        display: block !important;
+                  }
+            </style>`;
+            document.getElementsByClassName('fart')[0].remove();
+            document.getElementsByClassName('abcdefghijklmnopqrstuvwxyz')[0].remove();
+            clearInterval(animation);
       });
 }
-
-document.querySelectorAll(".material-symbols-outlined")[0].addEventListener("click", () => {
-      document.querySelector(".cover").style.display = "none";
-});
